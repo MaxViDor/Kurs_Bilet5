66deac1fdfe43ef6132b004bd2bed919408a5c1e
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-08-15 19:42:02 +0300

    add generation massive

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
 package src;
 
 public class Main {
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+    System.out.println(arr);
+}
+//        int [] arr = generateRandomArray();
+//        for (int i = 0; i < arr.length; i++)
+//            if (i % 2 == 0)
+//                i++;
+//        System.out.println(arr);
+//    }
 }
